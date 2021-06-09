55ad91c3b5568a6819cbbbd06d0e3423251d13cc
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-06-09 16:36:52 +0700

    Basic start with store,reducer,action,dispatch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+// Actions
+const increment = () => {
+  return { type: 'INCREMENT' };
+};
+
+const decrement = () => {
+  return { type: 'DECREMENT' };
+};
+
+// reducers
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+// Store
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
